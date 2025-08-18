3f2ef1c425ca423c9e3e99e7fa5973f6c7ed0a08
Ali Naci Erdem <anacierdem@gmail.com>
2025-08-18 16:27:03 +0000

    Initial commit - basic horse list

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HorseList from './components/HorseList.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HorseList />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/HorseList.vue
@@ -0,0 +1,116 @@
+<script setup>
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const horses = [
+  'Fast & Flurrious',
+  'Giddyup Giggles',
+  'Nacho Stallion',
+  'Donkey’s Cousin',
+  'Clip Clopocalypse',
+  'Hay Bale Houdini',
+  'Whinny the Poo',
+  'Turbo Haystack',
+  'Sir Gallops-a-Lot',
+  'Hay Fever',
+  'Neigh Sayer',
+  'Hoof Hearted',
+  'Trotzilla',
+  'Mane Attraction',
+  'Oats McGoats',
+  'Speedy Glue Stick',
+];
+
+const jockeys = [
+  'Bumblesnuff',
+  'Puddingworth',
+  'Jigglesby',
+  'Muffintop',
+  'Wigglebottom',
+  'Chumbleberry',
+  'Dinglewhip',
+  'Snortlepot',
+  'Wigglesworth',
+  'McSnort',
+  'Butterpants',
+  'Pickleback',
+  'Fumbleton',
+  'Snickerdoodle',
+  'Wobblekins',
+  'Crumpetbottom',
+];
+
+const colors = [
+  'antiquewhite',
+  'aquamarine',
+  'blueviolet',
+  'burlywood',
+  'coral',
+  'cornflowerblue',
+  'darkgray',
+  'darkgoldenrod',
+  'darkolivegreen',
+  'darkseagreen',
+  'darkslategray',
+  'deeppink',
+  'indianred',
+  'greenyellow',
+  'indigo',
+  'khaki',
+  'lightsalmon',
+  'lightseagreen',
+  'lightskyblue',
+  'mediumorchid',
+  'mediumspringgreen',
+];
+
+// Shuffle colors
+for (let i = 0; i < colors.length; i++) {
+  const randomIndex = getRandomInt(colors.length);
+  const tmp = colors[i];
+  colors[i] = colors[randomIndex];
+  colors[randomIndex] = tmp;
+}
+
+// TODO: do not use same names more than once
+const horseList = Array.from({ length: 20 }).map((_, i) => ({
+  name: `${horses[getRandomInt(horses.length)]} ${jockeys[getRandomInt(jockeys.length)]}`,
+  condition: Math.floor(Math.random() * 100 + 1),
+  color: colors[i],
+}));
+</script>
+
+<template>
+  <table>
+    <caption>
+      Horse List
+    </caption>
+    <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Condition</th>
+        <th scope="col">Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="horse in horseList">
+        <td scope="row">{{ horse.name }}</td>
+        <td>{{ horse.condition }}</td>
+        <!-- TODO: These have low color contrast -->
+        <td :style="{ backgroundColor: horse.color }">{{ horse.color }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+
+table,
+th,
+td {
+  border: 1px solid;
+}
+</style>
